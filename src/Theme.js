bcd20c3a084d7a6bafcab43678c3f1f11dab245e
Megan <at.fence.3@gmail.com>
2020-08-18 01:08:30 +0900

    add react-bootstrap, react-with-style

--- src/Theme.js
+++ src/Theme.js
@@ -1,13 +1,16 @@
 export default {
     color : {
-        primaryOrange: "#e17140", //주황
-        primaryGreen: "#3cd429", //녹색
-        white: "#efefef", //흰색
-        black: "#191918", //검정
+        primaryLight: "#e17140", //주황
+        primaryDark: "#3cd429", //녹색
+        backgroundLight: "#efefef", //흰색
+        backgroundDark: "#191918", //검정
         secondary: "#7a244a", //마젠타
     },
 
-    //font size
+    //font
+    font : {
+        main : 'Spoqa Han Sans',
+    },
     size : {
         xg : 24,
         lg : 18,
@@ -15,7 +18,6 @@ export default {
         sm : 12,
         xs : 10,
     },
-
     lineHeight : {
         xg : '60px',
         lg : '54px',
@@ -23,10 +25,12 @@ export default {
         sm : '24px',
         xs : '18px',
     },
-    
-    backgroundColor : {
-        default : "#FFFF00",
+    fontWeight : {
+        bold : 700,
+        medium : 300,
+        light : 100,
     },
 
     unit : 4,
+
 };
