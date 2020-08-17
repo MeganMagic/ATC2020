96d3bbb96f426670bb870d263c1951646c29be91
Megan <at.fence.3@gmail.com>
2020-08-17 15:33:03 +0900

    add react-with-styles

---
+++ src/Theme.js
@@ -0,0 +1,32 @@
+export default {
+    color : {
+        primaryOrange: "#e17140", //주황
+        primaryGreen: "#3cd429", //녹색
+        white: "#efefef", //흰색
+        black: "#191918", //검정
+        secondary: "#7a244a", //마젠타
+    },
+
+    //font size
+    size : {
+        xg : 24,
+        lg : 18,
+        md : 14,
+        sm : 12,
+        xs : 10,
+    },
+
+    lineHeight : {
+        xg : '60px',
+        lg : '54px',
+        md : '36px',
+        sm : '24px',
+        xs : '18px',
+    },
+    
+    backgroundColor : {
+        default : "#FFFF00",
+    },
+
+    unit : 4,
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import withStyles, { css } from '../withStyles';
+import { Container, Row, Col } from 'react-bootstrap';
 //import './Home.css';
 
 
-function Home(){
+function Home(props){
     return(
-        <div id = "main" className='container'>
+        <Container fluid {...css(props.styles.default)}>
+            <Row>
+                <Col>
+                    <div >
+                        ATC 2020
+                    </div>
+                    <div>AND</div>
+                </Col>
+            </Row>
 
-            <div className='title'>
-                ATC 2020<br/>AND
-            </div>
-
-            <div className='about'>
-                <p className='about-summary'>전시 소개가 들어가는 자리입니다. 대충 아무말 ATC 2020은 2020년도에 열리는 Art & Technology Conference의 줄임말로, 매해 아텍에서 개최되는 큰 행사입니다. 많은 아텍인들의 열정과 창의력을 엿볼 수 있습니다. 많은 관심 부탁드립니다.</p>
-                <div className='button'>
+            <Row>
+                <Col>
+                    <p className='about-summary'>전시 소개가 들어가는 자리입니다. 대충 아무말 ATC 2020은 2020년도에 열리는 Art & Technology Conference의 줄임말로, 매해 아텍에서 개최되는 큰 행사입니다. 많은 아텍인들의 열정과 창의력을 엿볼 수 있습니다. 많은 관심 부탁드립니다.</p>
                     <Link to="/about">전시 소개 더보기</Link>
-                </div>
-            </div>
+                </Col>
+            </Row>
 
-            <div className='button'>
-                <Link to="/tour">관람하기</Link>
-            </div>
+            <Row>
+                <Col>
+                    <div>
+                        <Link to="/tour">관람하기</Link>
+                    </div>
+                </Col>
+            </Row>
 
-            
-        </div>
+        </Container>
     );
 }
-export default Home;
+export default withStyles(({color, size, backgroundColor}) => ({
+    default: {
+        color : color.primaryOrange,
+        fontSize : size.xg,
+        backgroundColor : color.white,
+    },
+    
+}))(Home);
