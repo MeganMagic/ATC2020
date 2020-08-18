e869ebb1f6bde49a2b2a3503c330b9f626348b2b
Megan <at.fence.3@gmail.com>
2020-08-18 12:33:29 +0900

    change structure

--- src/components/ScrollTester.js
+++ src/components/ScrollTester.js
@@ -1,19 +1,56 @@
 import React from 'react';
 import '../css/ScrollTester.css';
 
+import _ from 'lodash';
+
 
 class ScrollTester extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            scrollTop : 0,
+            delayScrollTop : 0,
+            scrollDir : null,
+        };
+    }
+    componentDidMount(){
+        window.addEventListener('scroll', () => {
+            this.onScroll();
+            this.checkScrollDir();
+            }
+        );
+    }
 
     render(){
+        
         return(
             <div id="scroll-tester" >
                 <h1>SCROLL TEST</h1>
                 <br/>
-                <h4>현재 스크롤 위치(상단) : {this.props.scrollTop}</h4>
-                <h4>현재 보고있는 콘텐츠 번호 : {this.props.currContent}</h4>
+                <h4>현재 스크롤 위치(상단) : {this.state.scrollTop}</h4>
+                <h4>스크롤 방향 : {this.state.scrollDir}</h4>
             </div>
         );
     }
+
+    onScroll = (e) => {
+        const scrollTop = document.documentElement.scrollTop;
+        if (scrollTop !== this.state.scrollTop){
+            this.setState({scrollTop});
+        }
+    }
+    checkScrollDir = _.throttle( () => {
+        const newScrollTop = document.documentElement.scrollTop;
+        const scrollDiff = this.state.delayScrollTop - newScrollTop;
+
+        if (scrollDiff > 0 && this.state.scrollDir !== 'UP'){
+            this.setState({scrollDir : 'UP'});
+        }
+        else if (scrollDiff < 0 && this.state.scrollDir !== 'DOWN'){
+            this.setState({scrollDir : 'DOWN'});
+        }
+        this.setState({delayScrollTop : newScrollTop});
+    }, 500 )
 }
 
 export default ScrollTester;
