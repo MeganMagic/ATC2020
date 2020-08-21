3bdd8e95ee51161afce5d41994d69bfab3709e47
Megan <at.fence.3@gmail.com>
2020-08-21 12:35:12 +0900

    delete bootstrap, refactoring

--- src/routes/Tour3.js
+++ src/routes/Tour3.js
@@ -1,50 +1,50 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Data from '../data.json'
 import withStyles, { css } from '../withStyles';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import ScrollTester from '../components/ScrollTester';
 import GNB from '../components/GNB';
 
-import _ from 'lodash';
-
 class Tour3 extends React.Component{
-
-
     render(){
+        const style = this.props.styles;
         return(
-            <Container fluid>
+            <div id="Tour3" >
                 <ScrollTester />
 
                 <GNB />
 
                 {Data.map( (data, index) => (
-                    <Row>
-                        <Col>
-                            {data.id}<br/>
-                            {data.title}<br/>
+                    <div className="preview" key={index} {...css(style.preview)} >
+                        <div>{data.id}</div>
+                        <div>{data.title}</div>
+                        <div>
                             {data.artist}<br/>
                             {data.genre}<br/>
-                        </Col>
-                    </Row>
+                        </div>
+                        <div>
+                            <Link to={`/detail?id=${data.id}`}>Detail</Link>
+                        </div>
+                    </div>
                 ) )}
 
-            </Container>
+            </div>
 
         );
     }
 }
 
 export default withStyles(({color, size}) => ({
-    gnb : {
-        backgroundColor : color.primaryLight,
-        color : color.backgroundLight,
-        textAlign : "center",
-        position : "fixed",
-        zIndex : 5,
+    gnbSpace : {
+        height : "100px",
+    },
+    preview : {
+        backgroundColor : color.backgroundLight,
+        fontSize : size.xg,
+        textAlign : "left",
+        padding : "30px",
+        borderBottom : "solid black 3px",
     }
 
 }))(Tour3);
