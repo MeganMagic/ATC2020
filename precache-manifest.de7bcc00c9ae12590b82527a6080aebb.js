a1cea983104dd60742b0154249a3fda119af78b2
Megan <at.fence.3@gmail.com>
2020-09-15 19:09:30 +0900

    Updates

---
+++ precache-manifest.de7bcc00c9ae12590b82527a6080aebb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe30848cec4113770edb0fb521b5391",
+    "url": "/ATC2020/index.html"
+  },
+  {
+    "revision": "8aa6272db84b7b73201f",
+    "url": "/ATC2020/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6a1ca24f674067dd52c7",
+    "url": "/ATC2020/static/css/main.6c6e5cf5.chunk.css"
+  },
+  {
+    "revision": "8aa6272db84b7b73201f",
+    "url": "/ATC2020/static/js/2.7c1e5083.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ATC2020/static/js/2.7c1e5083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1ca24f674067dd52c7",
+    "url": "/ATC2020/static/js/main.c036a687.chunk.js"
+  },
+  {
+    "revision": "58ca1fb1a78ed2f79d2e",
+    "url": "/ATC2020/static/js/runtime-main.794431f0.js"
+  },
+  {
+    "revision": "00408b3cac8b9cafb830505e11c6474d",
+    "url": "/ATC2020/static/media/item-img.00408b3c.png"
+  },
+  {
+    "revision": "d0a1f9a26e51651b09fc0c3d86009d2e",
+    "url": "/ATC2020/static/media/sub-title-line.d0a1f9a2.png"
+  }
+]);
