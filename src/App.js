da220b0801aa83361a37b0e9ffeb524c51cb284a
Megan <at.fence.3@gmail.com>
2020-09-16 18:34:00 +0900

    add detail page route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './css/App.css';
 import Home2 from './routes/Home2';
 import Level from './routes/Level';
 import Level2 from './routes/Level2';
+import Detail from './routes/Detail';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" exact={true} component={Home2} />
       <Route path="/Level/:num" exact component={Level} />
       <Route path="/Level2" exact component={Level2} />
+      <Route path="/detail" component={Detail} />
 
     </HashRouter>
   );
