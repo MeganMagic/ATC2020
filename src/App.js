3e749502b75dadfadfc0018d612534ebe66735ed
Megan <at.fence.3@gmail.com>
2020-10-01 10:39:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { HashRouter, Route } from 'react-router-dom';
 import './css/App.css';
 
 import Home2 from './routes/Home2';
-
 import Level from './routes/Level';
+
+import Level6 from './routes/Level6';
 import Level2 from './routes/Level2';
 import Level1 from './routes/Level1';
 
@@ -17,8 +18,10 @@ function App() {
       <Route path="/" exact={true} component={Home2} />
       <Route path="/Level/:num" exact component={Level} />
       <Route path="/Level2" exact component={Level2} />
-      <Route path="/detail" component={Detail} />
       <Route path="/Level1" exact component={Level1} />
+      <Route path="/Level6" exact component={Level6} />
+
+      <Route path="/detail" component={Detail} />
 
     </HashRouter>
   );
