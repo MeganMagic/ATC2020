36e60bdf0c66ebb6f76de45787b10c1a446f6a50
Megan <at.fence.3@gmail.com>
2020-10-16 17:28:37 +0900

    Updates

---
+++ precache-manifest.4f24478fb8b4b6af50be866c522e4de8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255e8b9206f90c7214601cb7cbb95a3c",
+    "url": "/ATC2020/index.html"
+  },
+  {
+    "revision": "1dd27238293e08d46b60",
+    "url": "/ATC2020/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "67b482e45d155cc2f318",
+    "url": "/ATC2020/static/css/main.6447b6c6.chunk.css"
+  },
+  {
+    "revision": "1dd27238293e08d46b60",
+    "url": "/ATC2020/static/js/2.122643e4.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/ATC2020/static/js/2.122643e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b482e45d155cc2f318",
+    "url": "/ATC2020/static/js/main.4ebc7433.chunk.js"
+  },
+  {
+    "revision": "ace6a5beeada46b86e00",
+    "url": "/ATC2020/static/js/runtime-main.b8224b02.js"
+  },
+  {
+    "revision": "accbb055a67299e1ab827e0159c493e8",
+    "url": "/ATC2020/static/media/holder.accbb055.jpeg"
+  },
+  {
+    "revision": "b6a2f759f332aed073297e5f3c48d14c",
+    "url": "/ATC2020/static/media/item-img.b6a2f759.png"
+  },
+  {
+    "revision": "c5a83a1dff2b9e9249b30b8b46b0e16b",
+    "url": "/ATC2020/static/media/poster-temp.c5a83a1d.png"
+  },
+  {
+    "revision": "066ad6839358c63f216bf004133363bb",
+    "url": "/ATC2020/static/media/sub-title-line.066ad683.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d703482be1b16d5ad55fbf3d9db5440a.js"
+  "/ATC2020/precache-manifest.4f24478fb8b4b6af50be866c522e4de8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ATC2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
