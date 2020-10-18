6e87720f5b787832cb2ea5ff1e0a996db983eab1
Megan <at.fence.3@gmail.com>
2020-10-18 10:30:18 +0900

    Update Level6.js

--- src/routes/Level6.js
+++ src/routes/Level6.js
@@ -5,14 +5,14 @@ import MainFrame from '../components/MainFrame';
 import Item from '../components/Item';
 import { slice } from 'lodash';
 
- const parseGoogleSpreadsheetData = (data) => {
+ const parseGoogleSpreadsheetData = (data, level) => {
     const queryFrame = /google\.visualization\.Query\.setResponse\((.*)\)\;/;
     
     const myData = JSON.parse( queryFrame.exec( data.split('\n')[1] )['1'] ).table.rows.map((x)=>x.c);
     console.log(myData);
     const filteredData = myData
-    .filter((x) => x[0] !== null && x[0].v === this.state.level );
-    return myData;
+    .filter((x) => x[0] !== null && x[0].v === level );
+    return filteredData;
 }
 const parseGoogleDriveLink = (link) => {
     const queryFrame = /https:\/\/drive.google.com\/file\/d\/(.*)\/view/;
@@ -28,50 +28,9 @@ class Level6 extends React.Component{
         const keyLevel = '1i4DrB3mIM3yF6XUs_5hHO04-TGrbY2HtqUuJomCp-C4';
         const keyItem = '1lW9xsfxeghuknmVyrVNrkD9o-E9H4AbYiYy4dQ0aRCI';
         const level = 6;
-        let isLoading = true;
-        let isError = false;
-        let levelData;
-        let itemData;
 
-        //level 정보
-        axios.get( getUrl(keyLevel) ) 
-        .then( (res) => {
-        //1. data 가져오기, 가공
-            const { data } = res;
-            levelData = parseGoogleSpreadsheetData(data);
+        this.state = {keyLevel, keyItem, level, isLoading : true, isError : false};
 
-            isLoading = false;
-            isError = false;
-        })
-        //2. data error
-        .catch( (error) => {
-            console.log(error);
-            isLoading = false;
-            isError = true;
-        });
-
-        //작품 정보
-        axios.get( getUrl(keyItem) )
-        .then( (res) => {
-            const {data} = res;
-            itemData = parseGoogleSpreadsheetData(data);
-            isLoading = false;
-            isError = false;
-        })
-        .catch( (error) => {
-            console.log(error);
-            isError = true;
-        });
-
-        this.state = {
-            keyLevel,
-            keyItem,
-            level,
-            isLoading,
-            isError,
-            levelData,
-            itemData
-        };
     }
 
     
@@ -84,7 +43,7 @@ class Level6 extends React.Component{
             console.log(res);
             const { data } = res;
             console.log(data);
-            const levelData = parseGoogleSpreadsheetData(data);
+            const levelData = parseGoogleSpreadsheetData(data, this.state.level);
             console.log(levelData);
 
             //2. 데이터 저장, 로딩 종료
@@ -106,7 +65,7 @@ class Level6 extends React.Component{
         axios.get( getUrl(this.state.keyItem) )
         .then( (res) => {
             const {data} = res;
-            const itemData = parseGoogleSpreadsheetData(data);
+            const itemData = parseGoogleSpreadsheetData(data, this.state.level);
             this.setState({
                 itemData : itemData
             });
@@ -120,7 +79,7 @@ class Level6 extends React.Component{
     }
     
 
-    // componentDidMount(){ this.getData();}
+    componentDidMount(){ this.getData();}
     
 
     render(){
@@ -167,7 +126,20 @@ class Level6 extends React.Component{
                     entranceLevel = {data[2][2].v}
                     imgSrc = {'http://drive.google.com/uc?export=view&id=1UseOkQjZu9b25VLDTxupUJnTBVzEM3qi'}
                 />
-                {entrance(data[2])}
+                <Item entrance
+                    title={data[1][2].v}
+                    desc={data[1][3].v}
+
+                    entranceLevel = {data[2][2].v}
+                    imgSrc = {'http://drive.google.com/uc?export=view&id=1UseOkQjZu9b25VLDTxupUJnTBVzEM3qi'}
+                />
+                <Item entrance
+                    title={data[1][2].v}
+                    desc={data[1][3].v}
+
+                    entranceLevel = {data[2][2].v}
+                    imgSrc = {'http://drive.google.com/uc?export=view&id=1UseOkQjZu9b25VLDTxupUJnTBVzEM3qi'}
+                />
                 <li className="item-entrance">next</li>
 
             </Fragment>
