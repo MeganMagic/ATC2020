33fa8ab32f04515d86402d4ce4d03e06f2437d4e
Megan <at.fence.3@gmail.com>
2020-11-06 10:38:22 +0900

    Updates

--- precache-manifest.9c25262cdcf362de32c1341f63ffab51.js
+++ precache-manifest.9c25262cdcf362de32c1341f63ffab51.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d2f5e1a98bcb6c12314342ac795e924",
+    "revision": "afd9a6b3da881c51b818c25b9ecb4550",
     "url": "/ATC2020/index.html"
   },
   {
-    "revision": "1dd27238293e08d46b60",
+    "revision": "de66f4bb120eb4050875",
     "url": "/ATC2020/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "6e7c38746053b876eb85",
-    "url": "/ATC2020/static/css/main.6447b6c6.chunk.css"
+    "revision": "682fe7ab0e20871116cc",
+    "url": "/ATC2020/static/css/main.dc2fc280.chunk.css"
   },
   {
-    "revision": "1dd27238293e08d46b60",
-    "url": "/ATC2020/static/js/2.122643e4.chunk.js"
+    "revision": "de66f4bb120eb4050875",
+    "url": "/ATC2020/static/js/2.bbe68c5a.chunk.js"
   },
   {
     "revision": "64bea2e190f27355a76963b8cb94bb7b",
-    "url": "/ATC2020/static/js/2.122643e4.chunk.js.LICENSE.txt"
+    "url": "/ATC2020/static/js/2.bbe68c5a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e7c38746053b876eb85",
-    "url": "/ATC2020/static/js/main.8f9748f8.chunk.js"
+    "revision": "682fe7ab0e20871116cc",
+    "url": "/ATC2020/static/js/main.371fc256.chunk.js"
   },
   {
     "revision": "ace6a5beeada46b86e00",
@@ -34,6 +34,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b6a2f759f332aed073297e5f3c48d14c",
     "url": "/ATC2020/static/media/item-img.b6a2f759.png"
+  },
+  {
+    "revision": "441f4487e37c18ed98cca7a1b0924429",
+    "url": "/ATC2020/static/media/level5.441f4487.png"
+  },
+  {
+    "revision": "7d7b62a631ee55d39011e889447846da",
+    "url": "/ATC2020/static/media/level6.7d7b62a6.png"
+  },
+  {
+    "revision": "d04ae796df18cd3280eeba3c11acea1e",
+    "url": "/ATC2020/static/media/logo.d04ae796.png"
   },
   {
     "revision": "c5a83a1dff2b9e9249b30b8b46b0e16b",
