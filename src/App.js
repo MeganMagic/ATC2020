e05595734927488d2a97087cbaf7d98549317b70
Megan <at.fence.3@gmail.com>
2020-11-10 21:45:27 +0900

    studying browserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/App.css';
 
 import Home3 from './routes/Home3';
@@ -8,21 +8,21 @@ import Lobby2 from './routes/Lobby2';
 import Level1 from './routes/Level1';
 
 import Detail from './routes/Detail';
+import Gallery from './routes/Gallery';
 
 
 
 function App() {
   return (
-    <HashRouter>
-      <Route path="/" exact={true} component={Home3} />
-      <Route path="/Level1" exact component={Level1} />
-
-      <Route path="/Lobby2" exact component={Lobby2} />
-
-
-      <Route path="/detail" component={Detail} />
-
-    </HashRouter>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true} component={Home3} />
+        <Route path="/Level1" exact component={Level1} />
+        <Route path="/Lobby/:level" exact component={Lobby2} />
+        <Route path="/detail" component={Detail} />
+        <Route path="/Gallery/:workId" exact component={Gallery} />
+      </Switch>
+    </Router>
   );
 }
 
