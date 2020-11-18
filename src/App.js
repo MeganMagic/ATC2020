9ee9de1992af20b96daa81a3c0782eac730b21e4
Megan <at.fence.3@gmail.com>
2020-11-18 21:37:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { 
-  HashRouter as Router, 
-  Route, 
-  Switch
- } from 'react-router-dom';
+import { HashRouter, Route }from 'react-router-dom';
 import './css/App.css';
 
 import Home from './routes/Home3';
-import Lobby from './routes/Lobby2';
 import Level from './routes/Level';
 import Level1 from './routes/Level1';
-import Detail from './routes/Detail';
 import Gallery from './routes/Gallery';
-import Loading from './routes/Loading';
 
 
 
 function App() {
   return (
-    <Router >
-      <Switch>
+    <HashRouter>
         <Route exact path="/" component={Home} />
         <Route exact path="/level/:level" component={Level} />   
-        <Route exact path="/loading" component={Loading} />
         <Route exact path="/level/:level/gallery/:workID" component={Gallery} />
-      </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
